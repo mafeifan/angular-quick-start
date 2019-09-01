9b8e49433211e5f3b7632f28a1c6a5a85dbe47ea
finleyMa <maf@shinetechchina.com>
2019-09-01 15:37:30 +0800

    fix npm run test

--- Jenkinsfile
+++ Jenkinsfile
@@ -37,11 +37,10 @@ pipeline {
         stage("测试") {
             steps {
                 echo "单元测试中..."
-                sh 'cd app && npm run lint && npm run citest'
-                sh 'cd app && tar -zcvf coverage.tar.gz ./coverage/*'
-                sh 'cd app && tar -zcvf reports.tar.gz ./reports/*'
-                junit 'app/reports/*.xml'
-                archiveArtifacts artifacts: 'app/**/*.tar.gz', fingerprint: true
+                sh npm run lint && npm run citest'
+                sh tar -zcvf reports.tar.gz ./reports/*'
+                junit 'reports/*.xml'
+                archiveArtifacts artifacts: '**/*.tar.gz', fingerprint: true
                 echo "单元测试完成."
             }
         }
--- karma.conf.js
+++ karma.conf.js
@@ -10,23 +10,36 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-quick-start'),
+      dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit', 'coverage-istanbul'],
+    junitReporter: {
+      outputDir: require('path').join(__dirname, './reports'), // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'junit.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      useBrowserName: false // add browser name to report and classes names
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    customLaunchers: {
+      ChromeNoHead: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+        ]
+      }
+    },
+    singleRun: true
   });
 };
